75e45027f522376c222db01c18c766849eb9cb09
Nick Harding <100789789+colonelkablam@users.noreply.github.com>
2025-01-22 13:47:12 +0000

    Resolve __dirname for ES modules

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 const port = process.env.PORT || 3000
-
 const app = express()
+
+// Resolve __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
